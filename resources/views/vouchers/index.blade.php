de734b7d8d9ce8798b1bb5b59a7e25917cac11f7
Aidan McMillan <mcaidans@gmail.com>
2018-11-06 01:59:40 +0000

    pushing for transfer to website

--- resources/views/vouchers/index.blade.php
+++ resources/views/vouchers/index.blade.php
@@ -4,6 +4,50 @@
 
 <div class="row responsive-center">
     @if(!empty($vouchers))
+        <!-- MANUAL CHANGE DELETE -->
+        <div class="col-sm-6 col-lg-4 col-xl-3">
+                <div class="card" style=";margin-right:15px;padding:5px;">
+                    <a href="#voucher{{$t->id}}" role="button" data-toggle="modal">
+                        <img class="card-img-top" class="img-fluid" src="../storage/app/{{ $t->image_location }}" alt='{{$t->name}}'>
+                    </a>
+                    <div class="card-body text-center">
+                        <button type="button" class="btn greencode" data-toggle="modal" data-target="#voucher{{$t->id}}">Open Voucher</button>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="modal fade" id="voucher{{$t->id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <!-- <h5 class="modal-title" id="exampleModalLabel">{{ $t->name }}</h5> -->
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <img src="../storage/app/{{ $t->image_location }}" class="img-fluid" alt='{{$t->name}}'>
+                        </div>
+                        <div class="modal-footer">
+                            @if (Auth::check() && (Auth::user()->admin))
+                                <form style="margin-bottom:0px;" method="POST" action="{{ action('VoucherController@destroy', $t->id ) }}">
+                                    @csrf
+                                    @method("DELETE")
+                                    <input type="submit" class="btn btn-danger" href="{{ route('vouchers.destroy', $t->id) }}" value="Delete">
+                                </form>
+                                <a class="btn btn-warning" href="#">Edit</a>
+                            @else                    
+                                <button type="button" class="btn btn-primary">Redeem</button>
+                            @endif
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        
+        
+        <!-- END CHANGE DELETE -->
+        
         @foreach($vouchers as $voucher) 
             <div class="col-sm-6 col-lg-4 col-xl-3">
                 <div class="card" style=";margin-right:15px;padding:5px;">
